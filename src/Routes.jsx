44292e216080887ad5fd4b981a0138b0f8b1f213
piiivot <the_pivotsk8@hotmail.com>
2023-11-18 11:08:37 -0500

    feat view register & login

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import { LoginLayout } from "./layout/login.layout";
+import { LoginPage } from "./pages/login.page";
+import { RegisterPage } from "./pages/register.page";
+import { NotFoundPage } from "./pages/not-found.page";
 
 
 const routes = createBrowserRouter([
     {
         path: "/",
         element: <LoginLayout />,
+        errorElement: <NotFoundPage />,
+        children: [
+            {
+                path: "",
+                element: < LoginPage />,
+            },
+            {
+                path: "/register",
+                element: < RegisterPage />,
+            }
+
+        ]
     },
 ]);
 
--- src/pages/register.page.jsx
+++ src/pages/register.page.jsx
@@ -1,4 +1,4 @@
-export const LoginPage = () => {
+export const RegisterPage = () => {
     return (
 
         <div className="w-screen h-screen flex justify-center flex-col items-center ">
@@ -37,11 +37,22 @@ export const LoginPage = () => {
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Password
                             </label>
-                            <div className="text-sm">
-                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                    Forgot password?
-                                </a>
-                            </div>
+                        </div>
+                        <div className="mt-1">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
+                                placeholder="admin@example.com"
+                            />
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block mt-4 mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                                Confirma Password
+                            </label>
                         </div>
                         <div className="mt-2">
                             <input
@@ -61,7 +72,7 @@ export const LoginPage = () => {
                             type="submit"
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
-                            Sign in
+                            Crear Cuenta
                         </button>
                     </div>
                 </form>
@@ -70,5 +81,6 @@ export const LoginPage = () => {
         </div>
     )
 }
+
 
 
