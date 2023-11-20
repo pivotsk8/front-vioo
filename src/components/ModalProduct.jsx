351b29aa1c4ee15aeb93427812761a12c3be3563
piiivot <the_pivotsk8@hotmail.com>
2023-11-19 23:13:47 -0500

    fix : css details

--- src/components/ModalProduct.jsx
+++ src/components/ModalProduct.jsx
@@ -35,7 +35,7 @@ export const ProductModal = ({ selectedProduct, open, setOpen }) => {
                             <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                                 <button
                                     type="button"
-                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-900 text-base font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 sm:ml-3 sm:w-auto sm:text-sm"
                                     onClick={() => setOpen(false)}
                                 >
                                     Close
